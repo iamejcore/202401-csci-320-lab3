adcf93447d0b87c018a2e19abace32cc113ca385
iamejcore <77796467+iamejcore@users.noreply.github.com>
2023-11-20 05:06:06 +0000

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -9,8 +9,19 @@ int* worker_validation;
 int** read_board_from_file(char* filename){
     FILE *fp = NULL;
     int** board = NULL;
-
-    // replace this comment with your code
+if (fp != NULL) {
+        board = (int**)malloc(9 * sizeof(int*));
+        for (int row = 0; row < 9; row++) {
+            board[row] = (int*)malloc(9 * sizeof(int));
+            for (int col = 0; col < 9; col++) {
+                if (fscanf(fp, "%d", &board[row][col]) != 1) {
+                    fclose(fp);
+                    return NULL;
+                }
+            }
+        }
+        fclose(fp);
+    }
 
     return board;
 }
@@ -21,6 +32,6 @@ int is_board_valid(){
     pthread_attr_t attr;
     param_struct* parameter;
     
-    // replace this comment with your code
+   pthread_attr_init(&attr);
     
 }
