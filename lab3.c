6577d03e17737f690eb3d7999d89c1021a872c6a
iamejcore <77796467+iamejcore@users.noreply.github.com>
2023-12-01 03:51:26 +0000

    beginning of code

--- lab3.c
+++ lab3.c
@@ -3,5 +3,14 @@
 #include <stdlib.h>
 #include "lab3.h"
 
+extern int **sudoku_board;
+extern int *worker_validation;
 
+typedef struct {
+    int id;
+    int starting_row;
+    int ending_row;
+    int starting_col;
+    int ending_col;
+} param_struct;
 
