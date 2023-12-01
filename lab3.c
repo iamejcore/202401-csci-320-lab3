2ae3065b875ebf31b6f951327eeeb836e9493a2f
iamejcore <77796467+iamejcore@users.noreply.github.com>
2023-12-01 03:50:48 +0000

    started over

--- lab3.c
+++ lab3.c
@@ -3,27 +3,5 @@
 #include <stdlib.h>
 #include "lab3.h"
 
-extern int** sudoku_board;
-int* worker_validation;
-
-int** read_board_from_file(char* filename){
-    FILE *fp = NULL;
-    int** board = NULL;
-if (fp != NULL) {
-        board = (int**)malloc(9 * sizeof(int*));
-        for (int row = 0; row < 9; row++) {
-            board[row] = (int*)malloc(9 * sizeof(int));
-            for (int col = 0; col < 9; col++) {
-                if (fscanf(fp, "%d", &board[row][col]) != 1) {
-                    fclose(fp);
-                    return NULL;
-                }
-            }
-        }
-        fclose(fp);
-    }
-
-    return board;
-}
 
 
