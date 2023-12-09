2c588f4da007f3ac2e910d516b87c234372f5497
iamejcore <77796467+iamejcore@users.noreply.github.com>
2023-12-09 23:04:11 +0000

    Update lab3.c

--- lab3.c
+++ lab3.c
@@ -5,3 +5,10 @@
 
 extern int** sudoku_board;
 int* validation;
+
+int** read_board_from_file(char* filename){
+    FILE *fp = NULL;
+    sudoku_board = (int**)malloc(sizeof(int*)*ROW_SIZE);
+    for(int row = 0; row < ROW_SIZE; row++)
+	    sudoku_board[row] = (int*)malloc(sizeof(int)*COL_SIZE);
+    
